ab4b95bc69d0814d3971e6640500908632c9c8a3
Gurban Gurbanzada <gurban.gurbanzadeh@gmail.com>
2024-06-27 11:47:09 +0400

    js added

---
+++ 02-JsIntro/app.js
@@ -0,0 +1,185 @@
+"use strict";
+// console.log("Hello World! external");
+// // alert("Hello World! external");
+
+// // var let const
+
+// let ad = "John";
+
+// ad = "gurban";
+
+// const x = 9;
+
+// let surname = ad;
+
+// // x = 8;
+// console.log(ad);
+// console.log(x);
+// console.log(surname, "surname");
+
+// surname = "gurbanzada";
+// console.log(surname, "surname");
+
+// const person = {
+//   name: "John",
+//   surname: "Doe",
+//   age: 30,
+// };
+// // person.name = "Gurban";
+
+// console.log(person, "person");
+// // let person2 = { ...person };
+
+// console.log(person2, "person");
+// person2.name = "Gurban";
+// console.log(person2, "person2");
+// console.log(person, "person");
+// console.log(a, "a");
+
+// let a = 5;
+// var a = 8;
+// console.log(a, "a");
+
+// var a = 5;
+
+// {
+
+// }
+
+let a = 8;
+
+// a = "gurban";
+// console.log(a, "a");
+// console.log(typeof a, "a");
+
+let name = "John";
+name = "John";
+name = `John ${a}`;
+
+// console.log(name, "name");
+
+let check = true;
+check = false;
+
+// console.log(check, "check");
+// console.log(typeof check, "check");
+
+let result = null;
+
+let result2 = undefined;
+
+let b;
+
+let x = 19;
+let y = 5;
+
+result = x + y;
+result = x * y;
+result = x - y;
+result = x / y;
+result = x % y;
+
+// console.log(result, "result");
+
+name = "John";
+let surname = "Doe";
+
+let fullName = name + " " + surname;
+
+// console.log(fullName, "fullName");
+
+// console.log(name[0], "name 1-ci herf");
+
+name = "qurban";
+// console.log(name[0], "name 1-ci herf");
+// console.log(name.length, "name uzunluq");
+
+name = "kanan";
+// console.log(name[0], "name 1-ci herf");
+// console.log(name.length, "name uzunluq");
+// console.log(name[name.length - 1], "name uzunluq");
+
+x = 8;
+y = 0;
+result = x + name;
+
+result = x + y;
+result = y + x;
+
+result = x - y;
+
+result = x * y;
+result = x * y;
+result = x / y;
+
+// console.log(result, "result");
+
+x = 5;
+let i = 20;
+// if (x > i) {
+//   console.log(` x ${i}den boyukdur`);
+// } else {
+//   console.log(` x ${i}den kicikdir`);
+// }
+
+let first = 30;
+let second = 40;
+let third = 35;
+
+// if (first > second) {
+//   console.log("first boyukdur seconddan");
+// } else if (first == second) {
+//   console.log("beraberdir");
+// } else {
+//   console.log("first kicikdir seconddan");
+// }
+
+// if (first > second && first > third && second > third) {
+//   //   console.log(first, second, third);
+//   console.log(third, second, first);
+// } else if (second > first && second > third && third > first) {
+//   //   console.log(second, "second");
+//   //   console.log(second, third, first);
+//   console.log(first, third, second);
+// } else if (third > first && third > second && second > first) {
+//   //   console.log(third, "third");
+//   console.log(third, second, first);
+// } else {
+//   console.log(first, third, second);
+// }
+
+// if(true){
+
+// }
+// let num = 1234567;
+
+// let num = prompt("eded daxil edin");
+let num = 1;
+console.log(typeof num, "num");
+
+// num = `${num}`;
+// num = num.toNumber();
+if (num == num * 1) {
+  num = +num;
+}
+num = +num;
+switch (num) {
+  case 1:
+    console.log("yanvar");
+    break;
+  case 2:
+    console.log("fevral");
+    break;
+  case 3:
+    console.log("fevral");
+    break;
+
+  default:
+    console.log("duzgun ededi daxil edin");
+
+    break;
+}
+
+a > b ? console.log("a boyukdur bden") : console.log("a kicikdir bden");
+
+a > b && console.log("a>b");
